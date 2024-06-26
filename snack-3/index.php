5078402242f869c9d07b1c2cbb7aa3fcc25e6523
LorezoParis1996 <lorenzoparis86@live.it>
2024-06-26 15:18:17 +0200

    snack 3 completato

---
+++ snack-3/index.php
@@ -0,0 +1,61 @@
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php foreach($posts as $date => $postsList) {
+         echo "<h4>$date</h3>";
+         foreach($postsList as $post) {
+            echo "<p>{$post['title']}</p>";
+            echo "<p>{$post['author']}</p>";
+            echo "<p>{$post['text']}</p>";
+         }
+    } ?>
+</body>
+</html>
