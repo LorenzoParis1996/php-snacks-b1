ce98f742654e4a05131d9e15189e7bcf7c6500a7
LorezoParis1996 <lorenzoparis86@live.it>
2024-06-27 12:35:07 +0200

    utilizzato DIR

---
+++ snack-7/alunni.php
@@ -0,0 +1,34 @@
+<?php 
+
+$alunni = [
+    [
+        "name" => "gigi",
+        "lastname" => "gigio",
+        "grades" => [
+            10,
+            6,
+            8
+        ]
+    ],
+    [
+        "name" => "gina",
+        "lastname" => "ginetti",
+        "grades" => [
+            7,
+            6,
+            9
+        ]
+        
+    ],
+    [
+        "name" => "topo",
+        "lastname" => "lino",
+        "grades" => [
+            6,
+            5,
+            10
+        ]
+    ],
+];
+
+?>
---
+++ snack-7/index.php
@@ -0,0 +1,7 @@
+<?php 
+
+include_once __DIR__ . "/alunni.php";
+
+var_dump($alunni);
+
+?>
